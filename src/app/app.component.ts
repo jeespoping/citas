61f30576c8b6d3977f1ec4b6842baab46e945f88
manuel <manuellopezflorez134@gmail.com>
2021-04-13 20:20:00 -0500

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'citas';
+  listCitas: any[] = [];
+
+  agregarCita(cita: any){
+    this.listCitas.push(cita);
+  }
+
+  eliminarCitaListado(index: number){
+    this.listCitas.splice(index, 1);
+  }
 }
